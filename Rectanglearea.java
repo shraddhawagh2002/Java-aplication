d5d325775516ddbf8cc567f166073c046554625b
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:02:48 +0530

    Add files via upload

---
+++ Rectanglearea.java
@@ -0,0 +1,13 @@
+package com.shra;
+
+public class Rectanglearea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int W =5;
+		 int H =10;
+	     int area = W*H;
+	     System.out.println("area is "+area);
+	}
+
+}
