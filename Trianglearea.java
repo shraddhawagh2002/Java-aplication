4063833c15536e71b20f919a6a72ffc8b6a559f4
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:03:57 +0530

    Add files via upload

---
+++ Trianglearea.java
@@ -0,0 +1,13 @@
+package com.shra;
+
+public class Trianglearea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int b =5;
+		 int h =12;
+	     int area = (b*h)/2;
+	     System.out.println("area is "+area);
+	}
+
+}
