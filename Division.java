ade9fa6cb0e7ae2641da00f5885db45accc4a165
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 12:59:27 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,14 @@
+package com.shra;
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,s;
+	       i=15;
+	       j=5;
+	       s=i/j;
+	       System.out.println("the division of "+i+" and "+j+" is "+s);
+	}
+
+}
