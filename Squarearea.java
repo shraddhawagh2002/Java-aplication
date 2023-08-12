4d56c5d3b3fe18dc01c7690ee2a1ecfd3cca5a26
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:03:12 +0530

    Add files via upload

---
+++ Squarearea.java
@@ -0,0 +1,12 @@
+package com.shra;
+
+public class Squarearea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int s =14;
+      int area = s*s;
+      System.out.println("area is "+area);
+	}
+
+}
