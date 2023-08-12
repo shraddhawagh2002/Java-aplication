82ce6ef656b92888c29947314fa10449965c477d
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:07:32 +0530

    Add files via upload

---
+++ Ex3.java
@@ -0,0 +1,25 @@
+package nik.com;
+
+import java.util.Scanner;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter Number");
+		num=sc.nextInt();
+		if(num % 3==0 && num % 5 ==0)
+		{
+	      System.out.println(+num+"no is divisible by 3 & 5" );
+
+		}
+		else {
+			System.out.println(+num+"no is not divisible by 3 & 5");
+
+		}
+		
+	}
+
+}
