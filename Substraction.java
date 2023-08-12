8a475e47c07257566d5feb8abf150b76ff06b4bc
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:03:32 +0530

    Add files via upload

---
+++ Substraction.java
@@ -0,0 +1,14 @@
+package com.shra;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,s;
+	       i=30;
+	       j=20;
+	       s=i-j;
+	       System.out.println("the sub of "+i+" and "+j+" is "+s);
+	}
+
+}
