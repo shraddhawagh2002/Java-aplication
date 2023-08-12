4e14b8834dca7b555e83e679a69f0bd5a7790d51
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:07:55 +0530

    Add files via upload

---
+++ EX4.java
@@ -0,0 +1,25 @@
+package nik.com;
+
+import java.util.Scanner;
+
+public class EX4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter Number");
+		num=sc.nextInt();
+		if(num>=0)
+		{
+	      System.out.println("No is +ve");
+
+		}
+		else {
+			System.out.println("No is -ve");
+
+		}
+	}
+	
+
+}
