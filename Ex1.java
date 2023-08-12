cf9aa95642676719da3cc4579c5f6338d77b507e
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:08:26 +0530

    Add files via upload

--- Ex1.java
+++ Ex1.java
@@ -1,4 +1,4 @@
-package nik.com;
+package day7.com;
 
 import java.util.Scanner;
 
@@ -6,19 +6,29 @@ public class Ex1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num;
-		Scanner sc =new Scanner(System.in);
-		System.out.println("enter Number");
-		num=sc.nextInt();
-		if(num%2==0)
+    int a ,b ,c;
+    Scanner sc =new Scanner(System.in);
+	System.out.println("enter 1st Number");
+	a=sc.nextInt();
+	
+	System.out.println("enter 2nd  Number");
+	b=sc.nextInt();
+	
+	System.out.println("enter 3rd Number");
+	c=sc.nextInt();
+	
+	if(a>b && a>c){
+		System.out.println("the largest no is"+a);
+	}
+		else if(b>a && b>c)
 		{
-	      System.out.println("No is even");
-
+		System.out.println("the largest no is"+b);
 		}
-		else {
-			System.out.println("No is odd");
-
+		else
+		{
+			System.out.println("the largest no is"+c);
 		}
 	}
+	
 
 }
