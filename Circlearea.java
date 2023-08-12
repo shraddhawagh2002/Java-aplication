65a7028e974592757c2b42b36f88f25154cddccc
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 12:58:38 +0530

    Add files via upload

---
+++ Circlearea.java
@@ -0,0 +1,13 @@
+package com.shra;
+
+public class Circlearea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         double pi = 3.14;
+         int r = 5;
+         double area = pi*r*r;
+         System.out.println("area is "+area);
+	}
+
+}
