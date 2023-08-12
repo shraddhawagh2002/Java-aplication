b0fef9e7697b07ee98ac5f7c68f44cabcea0f530
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:01:24 +0530

    Add files via upload

---
+++ Module.java
@@ -0,0 +1,14 @@
+package com.shra;
+
+public class Module {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,s;
+	       i=15;
+	       j=2;
+	       s=i%j;
+	       System.out.println("the sum of "+i+" and "+j+" is "+s);
+	}
+
+}
