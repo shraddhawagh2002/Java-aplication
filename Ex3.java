ceef593627c1104ab139c4b99cefaba718a06d90
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:19:23 +0530

    Add files via upload

--- Ex3.java
+++ Ex3.java
@@ -1,4 +1,4 @@
-package nik.com;
+package day7.com;
 
 import java.util.Scanner;
 
@@ -6,20 +6,33 @@ public class Ex3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num;
-		Scanner sc =new Scanner(System.in);
-		System.out.println("enter Number");
-		num=sc.nextInt();
-		if(num % 3==0 && num % 5 ==0)
+		char Grade;
+	    Scanner sc =new Scanner(System.in);
+		System.out.println("enter grade");
+		Grade=sc.next().charAt(0);		
+		if((Grade=='A')||(Grade=='a'))
 		{
-	      System.out.println(+num+"no is divisible by 3 & 5" );
-
+			System.out.println("marks in between 90 to 100");
 		}
-		else {
-			System.out.println(+num+"no is not divisible by 3 & 5");
-
+		else if((Grade=='B')||(Grade=='b'))
+		{
+			System.out.println("marks in between 70 to 89");
 		}
-		
+		else if((Grade=='C')||(Grade=='c'))
+		{
+			System.out.println("marks in between 50 to 69");
+		}
+		else if((Grade=='D')||(Grade=='d'))
+		{
+			System.out.println("marks in between 0 to 49");
+		}
+		else
+		{
+			System.out.println("Invalid Grade");
+		}
+
 	}
+
+	
 
 }
