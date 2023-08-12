50953ebd20193cb9e110b229eddff6f7a305d6c3
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-12 13:06:52 +0530

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,24 @@
+package nik.com;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter Number");
+		num=sc.nextInt();
+		if(num%2==0)
+		{
+	      System.out.println("No is even");
+
+		}
+		else {
+			System.out.println("No is odd");
+
+		}
+	}
+
+}
