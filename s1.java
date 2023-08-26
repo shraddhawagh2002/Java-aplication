b8f598fdd03f43c7364b46a42e8a150120e9e198
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-26 15:35:40 +0530

    Add files via upload

---
+++ s1.java
@@ -0,0 +1,13 @@
+package day7.com;
+
+public class s1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int five = 5;
+		int two = 2;
+		int total = five + (five > 6 ? ++two : --two);
+	    System.out.println("total is " +total);
+	}
+
+}
