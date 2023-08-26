7b877bd26016cf9091b395f397e6764a3355ceae
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-26 15:28:11 +0530

    Add files via upload

---
+++ Loopex1.java
@@ -0,0 +1,14 @@
+package day7.com;
+
+public class Loopex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int i;
+    for(i=1;i<=100;i++)
+    {
+    	System.out.println("shraddha");
+    }
+	}
+
+}
