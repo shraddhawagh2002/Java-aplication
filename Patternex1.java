cfc1075cc3e1a969fa9455d0a32d0d81598e8abe
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-26 15:31:46 +0530

    Add files via upload

---
+++ Patternex1.java
@@ -0,0 +1,19 @@
+package day7.com;
+
+public class Patternex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     for(int i=10;i>=1;i--)//Row
+     {
+    	 for(int j=1;j<=i;j++)// column
+    	 {
+    		 //System.out.println("i="+i+"j="+j);
+    		 System.out.print("* ");
+
+    	 }
+    	 System.out.println();
+     }
+	}
+
+}
