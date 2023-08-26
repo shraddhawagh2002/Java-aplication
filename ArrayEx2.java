468062044314b91d82d6fe175a987bd47086fa6a
shraddhawagh2002 <89066569+shraddhawagh2002@users.noreply.github.com>
2023-08-26 15:15:10 +0530

    Add files via upload

---
+++ ArrayEx2.java
@@ -0,0 +1,25 @@
+package day7.com;
+
+import java.util.Scanner;
+
+public class ArrayEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[];
+		 Scanner sc=new Scanner(System.in);
+		 System.out.println("enter size of an aray :");
+		 int size=sc.nextInt();
+		 arr=new int[size];
+		 System.out.println("enter the ele  :");
+		 for(int i=0;i<arr.length;i++)
+		 {
+			 arr[i]=sc.nextInt();
+		 }
+		 System.out.println("entered ele are :");
+		 for(int i=0;i<arr.length;i++) {
+			 System.out.println(arr[i]);
+		 }
+	}
+
+}
